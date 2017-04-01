0f8b802aeb96aebcc15a60bc591cca69e219e7b0
Tobias Munk <tobias.munk@posteo.de>
2017-04-02 00:22:26 +0200

    replace tabs by spaces

--- home.php
+++ home.php
@@ -12,16 +12,16 @@ $content .= renderAlbumList($page->find("template=album,sort=-date,limit=20"));
 
 // if there are images, lets choose one to output in the sidebar
 if(count($page->images)) {
-	// if the page has images on it, grab one of them randomly...
-	$image = $page->images->getRandom();
-	// resize it to 400 pixels wide
-	$image = $image->width(400);
-	// output the image at the top of the sidebar...
-	$sidebar = "<img src='$image->url' alt='$image->description' />";
-	// ...and append sidebar text under the image
-	$sidebar .= $page->sidebar;
+  // if the page has images on it, grab one of them randomly...
+  $image = $page->images->getRandom();
+  // resize it to 400 pixels wide
+  $image = $image->width(400);
+  // output the image at the top of the sidebar...
+  $sidebar = "<img src='$image->url' alt='$image->description' />";
+  // ...and append sidebar text under the image
+  $sidebar .= $page->sidebar;
 } else {
-	// no images...
-	// append sidebar text if the page has it
-	$sidebar = $page->sidebar;
+  // no images...
+  // append sidebar text if the page has it
+  $sidebar = $page->sidebar;
 }
